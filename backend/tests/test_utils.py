b92edc49353d3f069ca4909a19775fbbb2d1a6bd
Jogisara Ramachandra Sandesh Bhat <sandeshbhatjr@gmail.com>
2020-04-11 08:12:05 +0200

    Change name of a repeated test name

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -47,7 +47,7 @@ class Test_group_contiguous_points:
 			(dt.date(2030,1,5), dt.date(2030,1,6))
 		]
 
-	def test_for_repeated_dates(self):
+	def test_for_repeated_same_date(self):
 		test_dates = [dt.date(2030,1,1), dt.date(2030,1,1), dt.date(2030,1,1), dt.date(2030,1,1), dt.date(2030,1,1)]
 		assert utils.group_contiguous_points(test_dates) == [
 			(dt.date(2030,1,1), dt.date(2030,1,1))
