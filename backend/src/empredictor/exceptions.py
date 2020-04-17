5ef2d3e00a1770cf1d8feab3e044e685d71ccfe7
Jogisara Ramachandra Sandesh Bhat <sandeshbhatjr@gmail.com>
2020-04-17 11:24:01 +0200

    Return dict error for HTTP handling

--- backend/src/empredictor/exceptions.py
+++ backend/src/empredictor/exceptions.py
@@ -1,5 +1,12 @@
 class RequestFailure(Exception):
-	pass
+	def __init__(self, message, status_code=None):
+		Exception.__init__(self)
+		self.message = message
+		if status_code is not None:
+			self.status_code = status_code
+	def to_dict(self):
+		res_dict = { 'msg' : self.message }
+		return res_dict
 
 class IncorrectAPIKey(Exception):
 	pass
